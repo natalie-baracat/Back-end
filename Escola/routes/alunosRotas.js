6ee934b61c2a258c5ad71398ffe1f736a836753d
natalie <nataliebaracatfranca@gmail.com>
2024-10-24 13:36:24 -0300

    tentando arrumar a parte de alunos (e falhando miseravelmente)

--- Escola/routes/alunosRotas.js
+++ Escola/routes/alunosRotas.js
@@ -11,4 +11,35 @@ router.get('/', async (req, res) => { //para acessar essa rota, digito /alunos/
     res.render('alunosTelas/lista', {alunos: buscaDados.rows})
 })
 
+//rota pra abrir tela para criar aluno (C - create)
+router.get('/novo', async (req, res) => { //para acessar essa rota, digito /disciplina/novo
+    try {
+        const resultado =  await BD.query('SELECT * FROM alunos ORDER BY nome')
+        res.render('alunosTelas/criar', {professoresCadastrados: resultado.rows})
+    } catch (erro) {
+        console.log('Erro ao abrir tela de cadastro de alunos', erro)
+        res.render('alunosTelas/criar', {mensagem: erro})
+    }
+})
+
+router.post('/novo', async (req, res) => {
+    try {
+        const {nome, email, idade, cpf} = req.body
+        await BD.query(`INSERT INTO alunos(nome, email, idade, cpf)
+                            VALUES($1, $2, $3, $4)`, [nome, email, idade, cpf])
+        res.redirect('/alunos')
+    } catch (erro) {
+        console.log('Erro ao cadastrar aluno', erro)
+        res.render('alunosTelas/criar', {mensagem: erro})
+    }
+})
+
+//rota para excluir uma aluno (D - delete)
+router.post('/:id/deletar', async (req, res) => { //para acessar digito turma/id/deletar
+    const {id} = req.params
+    await BD.query('DELETE FROM alunos WHERE id_aluno = $1', [id])
+    res.redirect('/alunos')
+}) 
+
+
 module.exports = router
