28b9e7198c17738fc3fed6332ddeaa7f74126549
natalie <nataliebaracatfranca@gmail.com>
2024-11-21 08:44:42 -0300

    adicionando sistema de paginação nas disciplinas

--- Escola/routes/disciplinasRotas.js
+++ Escola/routes/disciplinasRotas.js
@@ -8,18 +8,37 @@ router.get('/', async (req, res) => { //para acessar essa rota, digito /discipli
     try {
         const busca = req.query.busca || ''
         const ordenar = req.query.ordenar || 'nome_disciplina'
+        const pg = req.query.pg || 1 //obtendo a pag de dados
+
+        const limite = 5; //numero de registros por pagina
+        const offset = (pg - 1) * limite //qtd de registros de quero "pular"
+
         const buscaDados = await BD.query(`
             SELECT d.id_disciplina, d.nome_disciplina, p.nome_professor
                 FROM disciplinas AS d
                     LEFT JOIN professores AS p ON d.id_professor = p.id_professor
                     WHERE upper(d.nome_disciplina) like $1 or upper(p.nome_professor) like $1
-                ORDER BY ${ordenar}`, [`%${busca.toUpperCase()}%`]
-            )
+                ORDER BY ${ordenar}
+                LIMIT $2 OFFSET $3`, [`%${busca.toUpperCase()}%`, limite, offset]
+        )
+
+        const totalItens = await BD.query(`
+            SELECT COUNT(*) AS total
+                FROM disciplinas AS d
+                    LEFT JOIN professores AS p ON d.id_professor = p.id_professor
+                    WHERE upper(d.nome_disciplina) like $1 or upper(p.nome_professor) like $1`
+        )
+
+        const totalPgs = Math.ceil(totalItems.rows[0].total / limite)
+
         res.render('disciplinasTelas/lista', {
             vetorDados: buscaDados.rows,
             busca: busca,
-            ordenar: ordenar
+            ordenar: ordenar,
+            pgAtual : parseInt(pg),
+            totalPgs : totalPgs
         })
+        
     } catch (erro) {
         console.log('Erro ao listar disciplinas', erro)
         res.render('disciplinasTelas/lista', {mensagem: erro})
